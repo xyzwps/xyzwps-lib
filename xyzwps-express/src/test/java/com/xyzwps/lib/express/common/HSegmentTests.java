9eeb7f6831f82ff3ee6e7cd3ec827a96a71764a7
亮玉 韦 <weily@yingmi.cn>
2024-04-16 09:47:48 +0800

    [express] test hsegment

---
+++ xyzwps-express/src/test/java/com/xyzwps/lib/express/common/HSegmentTests.java
@@ -0,0 +1,34 @@
+package com.xyzwps.lib.express.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static com.xyzwps.lib.express.common.HSegment.*;
+
+class HSegmentTests {
+
+    static final Class<IllegalArgumentException> IAE = IllegalArgumentException.class;
+
+    @Test
+    void create() {
+        assertEquals(StarSegment.INSTANCE, from("*"));
+        assertEquals(Star2Segment.INSTANCE, from("**"));
+        assertEquals(new VariableSegment("abc"), from("{abc}"));
+        assertEquals(new PlainSegment("abc"), from("abc"));
+        assertEquals(new PlainSegment("aB_1"), from("aB_1"));
+
+        assertEquals("Segment string cannot be empty", assertThrows(IAE, () -> from("")).getMessage());
+        assertEquals("Segment string cannot be empty", assertThrows(IAE, () -> from(null)).getMessage());
+
+        assertEquals("Invalid path variable segment '{'", assertThrows(IAE, () -> from("{")).getMessage());
+        assertEquals("Invalid path variable segment '{ddd'", assertThrows(IAE, () -> from("{ddd")).getMessage());
+
+        assertEquals("Invalid path variable segment '{}'", assertThrows(IAE, () -> from("{}")).getMessage());
+        assertEquals("Invalid path variable name ' '", assertThrows(IAE, () -> from("{ }")).getMessage());
+        assertEquals("Invalid path variable name '*'", assertThrows(IAE, () -> from("{*}")).getMessage());
+
+        assertEquals("Invalid segment 'a*'", assertThrows(IAE, () -> from("a*")).getMessage());
+
+        assertEquals("Invalid path segment 'a?'", assertThrows(IAE, () -> from("a?")).getMessage());
+    }
+}
