2c65d016c6f6e1926e6fefa0fbc895df964ae1cb
亮玉 韦 <weily@yingmi.cn>
2024-05-14 10:01:36 +0800

    [website] spa

---
+++ xyzwps-website/src/main/react/src/App.tsx
@@ -0,0 +1,44 @@
+import { Outlet, RouterProvider, Link, createRouter, createRoute, createRootRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import IndexPage from './pages/IndexPage';
+import AboutPage from './pages/AboutPage';
+import './App.css';
+
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <div>
+        <Link to="/">Home</Link> <Link to="/about">About</Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: IndexPage,
+});
+
+const aboutRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/about',
+  component: AboutPage,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
---
+++ xyzwps-website/src/main/react/src/pages/AboutPage.tsx
@@ -0,0 +1,3 @@
+export default function AboutPage() {
+  return <div className="p-2">Hello from About!</div>;
+}
