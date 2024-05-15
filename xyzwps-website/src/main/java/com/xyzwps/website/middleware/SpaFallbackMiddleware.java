23d00fe731678a97226d96feed4f23a42f25a619
亮玉 韦 <weily@yingmi.cn>
2024-05-15 17:58:03 +0800

    [website] refactor spa fallback middleware

--- xyzwps-website/src/main/java/com/xyzwps/website/middleware/SpaFallbackMiddleware.java
+++ xyzwps-website/src/main/java/com/xyzwps/website/middleware/SpaFallbackMiddleware.java
@@ -5,16 +5,18 @@ import com.xyzwps.lib.express.HttpHeaders;
 import com.xyzwps.lib.express.HttpMethod;
 import com.xyzwps.lib.express.HttpMiddleware;
 import com.xyzwps.website.modules.conf.Configurations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Singleton
 public class SpaFallbackMiddleware implements HttpMiddleware {
+
+    private static final Logger log = LoggerFactory.getLogger(SpaFallbackMiddleware.class);
 
     private final Configurations conf;
 
@@ -27,23 +29,31 @@ public class SpaFallbackMiddleware implements HttpMiddleware {
     public void call(HttpContext context) {
         var req = context.request();
         if (req.method() == HttpMethod.GET && !req.path().startsWith("/api")) {
-            var path = Path.of(conf.getRouterStaticDirectory()).resolve("index.html");
-            if (Files.exists(path)) {
-                try {
-                    var bytes = Files.readAllBytes(path);
-                    var resp = context.response();
-                    var headers = resp.headers();
-                    headers.set(HttpHeaders.CONTENT_TYPE, "text/html");
-                    context.response().send(bytes);
-                } catch (IOException e) {
-                    throw new UncheckedIOException(e); // TODO: 处理错误
-                }
-            } else {
-                context.next();
+            var bytes = getIndexDotHtml();
+            if (bytes != null) {
+                var resp = context.response();
+                var headers = resp.headers();
+                headers.set(HttpHeaders.CONTENT_TYPE, "text/html");
+                context.response().send(bytes);
+                return;
             }
-        } else {
-            context.next();
         }
+
+        context.next();
     }
 
+    private byte[] getIndexDotHtml() {
+        var path = Path.of(conf.getRouterStaticDirectory()).resolve("index.html");
+        if (!Files.exists(path)) {
+            log.warn("File {} does not exist.", path);
+            return null;
+        }
+
+        try {
+            return Files.readAllBytes(path);
+        } catch (Exception e) {
+            log.error("Read file {} error.", path, e);
+            return null;
+        }
+    }
 }
